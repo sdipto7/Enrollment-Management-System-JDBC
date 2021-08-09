bf347ccfc824b65b593e28dd68bc35e9751ca2ba
sdipto7 <rumi.dipto@therapservices.net>
2021-08-09 20:08:09 +0600

    Added to printing methods to display trainee and course info

--- src/main/java/net/therap/jdbc/service/JdbcViewService.java
+++ src/main/java/net/therap/jdbc/service/JdbcViewService.java
@@ -1,8 +1,11 @@
 package net.therap.jdbc.service;
 
+import net.therap.jdbc.domain.Course;
 import net.therap.jdbc.domain.EnrollmentDetailsOutput;
+import net.therap.jdbc.domain.Trainee;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -10,6 +13,20 @@ import java.util.Map;
  * @since 8/9/21
  */
 public class JdbcViewService {
+
+    public static void printCourseInformation(List<Course> courseList) {
+        System.out.println("Courses: ");
+        for (Course c : courseList) {
+            System.out.println(c.getCourseCode() + " - " + c.getCourseTitle());
+        }
+    }
+
+    public static void printTraineeInformation(List<Trainee> traineeList) {
+        System.out.println("Trainees: ");
+        for (Trainee t : traineeList) {
+            System.out.println(t.getTraineeId() + " - " + t.getTraineeName());
+        }
+    }
 
     public static void print(EnrollmentDetailsOutput enrollmentDetailsOutput) {
         for (Map.Entry m : enrollmentDetailsOutput.getTraineeInfo().entrySet()) {
