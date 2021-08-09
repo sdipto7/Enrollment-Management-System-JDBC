55eb1ac05f2308c07a27a75776a76516db8be828
sdipto7 <rumi.dipto@therapservices.net>
2021-08-09 20:06:40 +0600

    Updated the executeOperation method

--- src/main/java/net/therap/jdbc/controller/JdbcController.java
+++ src/main/java/net/therap/jdbc/controller/JdbcController.java
@@ -1,14 +1,13 @@
 package net.therap.jdbc.controller;
 
-import net.therap.jdbc.domain.Database;
-import net.therap.jdbc.domain.EnrollmentDetails;
-import net.therap.jdbc.domain.EnrollmentDetailsOutput;
+import net.therap.jdbc.domain.*;
 import net.therap.jdbc.service.JdbcService;
 import net.therap.jdbc.service.JdbcViewService;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * @author rumi.dipto
@@ -22,16 +21,44 @@ public class JdbcController {
 
         Connection connection = JdbcService.connectDatabase(database);
 
-        String query = "select trainee_table.Trainee_id, trainee_table.Trainee_Name, course_table.Course_Code, course_table.Course_Title FROM " +
-                "((enrollment_table inner join trainee_table on enrollment_table.Trainee_id=trainee_table.Trainee_id) " +
-                "inner join course_table on course_table.Course_Code=enrollment_table.Course_Code)";
+        Scanner input = new Scanner(System.in);
 
-        ResultSet resultSet = JdbcService.queryExecute(connection, query);
+        System.out.println("Press 1 to view courses");
+        System.out.println("Press 2 to view trainees");
+        System.out.println("Press 3 to view course enrollment details");
+        System.out.println("Press 4 to assign a course to trainee");
 
-        List<EnrollmentDetails> enrollmentDetailsList = JdbcService.enrollmentDataExtract(resultSet);
+        int operation = input.nextInt();
+        executeOperation(connection, operation);
+    }
 
-        EnrollmentDetailsOutput enrollmentDetailsOutput = JdbcService.generateOutput(enrollmentDetailsList);
-
-        JdbcViewService.print(enrollmentDetailsOutput);
+    public static void executeOperation(Connection connection, int operation){
+        String query = "";
+        ResultSet resultSet = null;
+        switch (operation){
+            case 1:
+                query = "SELECT * FROM course_table";
+                resultSet = JdbcService.queryExecute(connection, query);
+                List<Course> courseList = JdbcService.courseDataExtract(resultSet);
+                JdbcViewService.printCourseInformation(courseList);
+                break;
+            case 2:
+                query = "SELECT * FROM trainee_table";
+                resultSet = JdbcService.queryExecute(connection, query);
+                List<Trainee> traineeList = JdbcService.traineeDataExtract(resultSet);
+                JdbcViewService.printTraineeInformation(traineeList);
+                break;
+            case 3:
+                query = "select trainee_table.Trainee_id, trainee_table.Trainee_Name, course_table.Course_Code, course_table.Course_Title FROM " +
+                        "((enrollment_table inner join trainee_table on enrollment_table.Trainee_id=trainee_table.Trainee_id) " +
+                        "inner join course_table on course_table.Course_Code=enrollment_table.Course_Code)";
+                resultSet = JdbcService.queryExecute(connection, query);
+                List<EnrollmentDetails> enrollmentDetailsList = JdbcService.enrollmentDataExtract(resultSet);
+                EnrollmentDetailsOutput enrollmentDetailsOutput = JdbcService.generateOutput(enrollmentDetailsList);
+                JdbcViewService.print(enrollmentDetailsOutput);
+                break;
+            case 4:
+                break;
+        }
     }
 }
