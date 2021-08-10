bf2cadead4d6241f192e1510eb7ab6ba2b5f067f
sdipto7 <rumi.dipto@therapservices.net>
2021-08-10 14:39:34 +0600

    Added this interface to store the database information

---
+++ src/main/java/net/therap/jdbc/util/DatabaseConstants.java
@@ -0,0 +1,14 @@
+package net.therap.jdbc.util;
+
+/**
+ * @author rumi.dipto
+ * @since 8/10/21
+ */
+public interface DatabaseConstants {
+
+    public static final String URL = "jdbc:mysql://localhost:3306/course_enrollment_details";
+
+    public static final String USERNAME = "root";
+
+    public static final String PASSWORD = "welcome987";
+}
