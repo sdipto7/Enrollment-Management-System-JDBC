0a4f2132310b0a9a9fea21a18bc041023345b37c
sdipto7 <rumi.dipto@therapservices.net>
2021-08-10 15:30:07 +0600

    Removed the generateOutput method

--- src/main/java/net/therap/jdbc/service/EnrollmentService.java
+++ src/main/java/net/therap/jdbc/service/EnrollmentService.java
@@ -1,30 +1,18 @@
 package net.therap.jdbc.service;
 
-import net.therap.jdbc.domain.*;
-import net.therap.jdbc.util.Database;
+import net.therap.jdbc.domain.Course;
+import net.therap.jdbc.domain.Enrollment;
+import net.therap.jdbc.domain.Trainee;
 
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 
 /**
  * @author rumi.dipto
  * @since 8/9/21
  */
-public class JdbcService implements Database {
-
-    public static Connection connectDatabase() {
-        Connection connection = null;
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }
-        return connection;
-    }
+public class EnrollmentService {
 
     public static ResultSet queryExecute(Connection connection, String query) {
         ResultSet resultSet = null;
@@ -34,89 +22,81 @@ public class JdbcService implements Database {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
         return resultSet;
     }
 
     public static void updateExecute(Connection connection, String query, String traineeId, String courseCode) {
-        try{
+        try {
             PreparedStatement preparedStatement = connection.prepareStatement(query);
             preparedStatement.setString(1, traineeId);
             preparedStatement.setString(2, courseCode);
             int row = preparedStatement.executeUpdate();
             System.out.println(row + " rows affected!");
-        }catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public static List<Enrollment> enrollmentDataExtract(ResultSet resultSet) {
-        List<Enrollment> enrollmentDetailsList = new ArrayList<>();
+    public static List<Enrollment> extractEnrollmentData(ResultSet resultSet) {
+        List<Enrollment> enrollmentList = new ArrayList<>();
 
         try {
             while (resultSet.next()) {
-                Enrollment e = new Enrollment();
-                e.setCourseCode(resultSet.getString("course_table.Course_Code"));
-                e.setCourseTitle(resultSet.getString("course_table.Course_Title"));
-                e.setTraineeName(resultSet.getString("trainee_table.Trainee_Name"));
-                e.setTraineeId(resultSet.getString("trainee_table.Trainee_id"));
-                enrollmentDetailsList.add(e);
+                boolean newEnrollment = true;
+
+                Course course = new Course(resultSet.getString("course.course_code"), resultSet.getString("course.course_title"));
+                Trainee trainee = new Trainee(resultSet.getString("trainee.trainee_id"), resultSet.getString("trainee.trainee_name"));
+
+                if (enrollmentList.size() > 0) {
+                    for (Enrollment enrollment : enrollmentList) {
+                        if (enrollment.getTrainee().getTraineeId().equals(resultSet.getString("trainee.trainee_id"))) {
+                            enrollment.getCourseList().add(course);
+                            newEnrollment = false;
+                            break;
+                        }
+                    }
+                }
+                if (newEnrollment) {
+                    List<Course> courseList = new ArrayList<>();
+                    courseList.add(course);
+                    enrollmentList.add(new Enrollment(trainee, courseList));
+                }
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return enrollmentDetailsList;
+
+        return enrollmentList;
     }
 
-    public static List<Trainee> traineeDataExtract(ResultSet resultSet) {
+    public static List<Trainee> extractTraineeData(ResultSet resultSet) {
         List<Trainee> traineeList = new ArrayList<>();
 
         try {
             while (resultSet.next()) {
-                Trainee t = new Trainee(resultSet.getString("Trainee_id"), resultSet.getString("Trainee_Name"));
-                traineeList.add(t);
+                Trainee trainee = new Trainee(resultSet.getString("trainee_id"), resultSet.getString("trainee_name"));
+                traineeList.add(trainee);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
         return traineeList;
     }
 
-    public static List<Course> courseDataExtract(ResultSet resultSet) {
+    public static List<Course> extractCourseData(ResultSet resultSet) {
         List<Course> courseList = new ArrayList<>();
 
         try {
             while (resultSet.next()) {
-                Course c = new Course(resultSet.getString("Course_Code"), resultSet.getString("Course_Title"));
-                courseList.add(c);
+                Course course = new Course(resultSet.getString("course_code"), resultSet.getString("course_title"));
+                courseList.add(course);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
         return courseList;
-    }
-
-    public static void generateOutput(List<Enrollment> enrollmentDetailsList) {
-        HashMap<String, HashSet<String>> enrollmentInfo = new HashMap<>();
-
-        HashMap<String, String> traineeInfo = new HashMap<>();
-
-        HashMap<String, String> courseInfo = new HashMap<>();
-
-        for (Enrollment e : enrollmentDetailsList) {
-            String courseCode = e.getCourseCode();
-            String courseTitle = e.getCourseTitle();
-            String traineeName = e.getTraineeName();
-            String traineeId = e.getTraineeId();
-
-            traineeInfo.put(traineeId, traineeName);
-            courseInfo.put(courseCode, courseTitle);
-            if (enrollmentInfo.containsKey(traineeId)) {
-                enrollmentInfo.get(traineeId).add(courseCode);
-            } else {
-                enrollmentInfo.put(traineeId, new HashSet<>());
-                enrollmentInfo.get(traineeId).add(courseCode);
-            }
-        }
-        JdbcViewService.print(enrollmentInfo, traineeInfo, courseInfo);
     }
 }
