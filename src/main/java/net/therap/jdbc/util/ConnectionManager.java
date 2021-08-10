7182f2e0bedfccdb57e859af6bae4e6ef06bcf3c
sdipto7 <rumi.dipto@therapservices.net>
2021-08-11 01:18:05 +0600

    Updated the constant variable access way

--- src/main/java/net/therap/jdbc/util/ConnectionManager.java
+++ src/main/java/net/therap/jdbc/util/ConnectionManager.java
@@ -4,11 +4,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import static net.therap.jdbc.util.DatabaseConstants.*;
+
 /**
  * @author rumi.dipto
  * @since 8/10/21
  */
-public class ConnectionManager implements DatabaseConstants {
+public class ConnectionManager {
 
     public static Connection getConnection() {
         Connection connection = null;
