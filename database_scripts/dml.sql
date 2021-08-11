933504ce7c8f01dcfc2aa9aca1c545018ad4035e
sdipto7 <rumi.dipto@therapservices.net>
2021-08-11 13:38:16 +0600

    Removed the select statements

--- database_scripts/dml.sql
+++ database_scripts/dml.sql
@@ -5,32 +5,20 @@ INSERT INTO course VALUES
 ("CS204", "Introduction to JDBC with MySql");
 
 INSERT INTO trainee VALUES
-("TR0001", "SHAHRIAR"),
-("TR0002", "AYON"),
-("TR0003", "FAHIM"),
-("TR0004", "NAVEED"),
-("TR0005", "SUMON");
+(101, "SHAHRIAR"),
+(102, "AYON"),
+(103, "FAHIM"),
+(104, "NAVEED"),
+(105, "SUMON");
 
 INSERT INTO enrollment VALUES
-("TR0001", "CS204"),
-("TR0001", "CS203"),
-("TR0001", "CS202"),
-("TR0002", "CS204"),
-("TR0002", "CS202"),
-("TR0003", "CS203"),
-("TR0003", "CS201"),
-("TR0003", "CS202"),
-("TR0004", "CS202"),
-("TR0004", "CS203"),
-("TR0004", "CS204"),
-("TR0005", "CS204"),
-("TR0005", "CS201");
-
-
-SELECT * FROM course;
-
-SELECT * FROM trainee;
-
-SELECT trainee.trainee_id, trainee.trainee_name, course.course_code, course.course_title FROM ((enrollment INNER JOIN trainee ON enrollment.trainee_id=trainee.trainee_id) INNER JOIN course ON course.course_code=enrollment.course_code);
-
-INSERT INTO enrollment VALUES (?, ?);
+(101, "CS203"),
+(101, "CS201"),
+(102, "CS204"),
+(102, "CS202"),
+(103, "CS203"),
+(103, "CS201"),
+(104, "CS202"),
+(104, "CS203"),
+(105, "CS204"),
+(105, "CS201");
